2c3bd709aacb5e256dd0626d256936667d176dde
Sebastian Vaca <svaca@broadinstitute.org>
2019-05-04 18:15:06 -0400

    Added read_and_hash_index_parquetFile

--- src/final_project/AvG_luigi_pipeline.py
+++ src/final_project/AvG_luigi_pipeline.py
@@ -27,9 +27,30 @@ def hash_str(some_val, salt=''):
 def hash_value(some_val, salt=''):
     return hash_str(some_val, salt=salt).hex()[:8]
 
+def read_and_hash_index_parquetFile(input_path, path ):
+
+    df = pq.read_pandas(input_path).to_pandas()
+
+    # Concatenate values from multiple columns to create a unique identifier for each
+    # Analyte, transition (signal) and MS acquisition (Mass spectrometry analysis)
+
+    df['ID_Analyte'] = df['Protein Name'].astype(str) + '_' + df['Peptide Modified Sequence'].astype(str) + '_' + \
+                       df['Precursor Charge'].astype(str) + df['Is Decoy'].astype(str)
+    df['ID_FragmentIon_charge'] = df['Fragment Ion'].astype(str) + '_' + df['Product Charge'].astype(str)
+
+    # Hashed the values to obtain the unique identifier
+    df['ID_Analyte'] = df['ID_Analyte'].map(lambda x: hash_value(x))
+    df['ID_FragmentIon_charge'] = df['ID_FragmentIon_charge'].map(lambda x: hash_value(x))
+    df['ID_Rep'] = df['File Name'].astype(str).map(lambda x: hash_value(x))
+
+    table = pa.Table.from_pandas(df)
+    pq.write_to_dataset(table,
+                        root_path= path,
+                        partition_cols=['ID_Analyte'])
+
 class SaltString():
     @staticmethod
-    def get_hash_of_file(filename, salt=''):
+    def get_hash_of_file(filename):
         try:
             hasher = hashlib.md5()
             with open(filename, 'rb') as input_file:
@@ -80,12 +101,25 @@ class ConvertCSVToParquet(luigi.Task):
             # Write CSV chunk to the parquet file
             table = pa.Table.from_pandas(chunk, schema=parquet_schema)
             parquet_writer.write_table(table)
+            parquet_writer.close()
 
-        parquet_writer.close()
+
+class ReadAndHashIndexParquetFile(luigi.Task):
+    file_stem_reduce = luigi.Parameter(default='reduced_data')
+
+    def requires(self):
+        return ConvertCSVToParquet()
+    def output(self):
+        hex_tag = SaltString.get_hash_of_file(self.input().path)
+        return luigi.LocalTarget(self.file_stem_reduce+"_%s.txt" % hex_tag)
+    def run(self):
+        read_and_hash_index_parquetFile(input_path=self.input().path,
+                                        path='data/pq_ds/')
 
 
 class RTask(luigi.Task):
     file_stem_rtask=luigi.Parameter(default='rtask_data')
+
     def requires(self):
         return MapStep()
 
