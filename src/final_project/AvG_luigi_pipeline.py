d4a09ef6ad225d86a54165269a4cde61ea910c6f
Sebastian Vaca <svaca@broadinstitute.org>
2019-05-05 13:04:09 -0400

    Adapted the RTask to work on all analytes

--- src/final_project/AvG_luigi_pipeline.py
+++ src/final_project/AvG_luigi_pipeline.py
@@ -184,24 +184,25 @@ class RTask_AvantGarde(luigi.Task):
                 'ID_analyte_glossary': self.clone(TransformParquetPartitionsToCSV)}
 
     def output(self):
-        hex_tag = SaltString.get_hash_of_file(self.input()['params_file'].path)
-        return luigi.LocalTarget(self.file_stem_rtask + "_%s.txt" % hex_tag)
+        hex_tag = SaltString.get_hash_of_file(self.input()['ID_analyte_glossary'].path)
+        return luigi.LocalTarget(self.output_dir + 'ID_Analyte_glossary_3' + "_%s.csv" % hex_tag)
 
     def run(self):
-        # input_path = self.input()['ID_analyte_glossary'].path
+        input_path = self.input()['ID_analyte_glossary'].path
         params_file_path = self.input()['params_file'].path
 
-        subprocess_call_for_r_script = generate_subprocess_call_for_a_analyte(
-            hashed_id='0b59f29e',
-            csv_ds_root_path=self.csv_ds_root_path,
-            params_file_path=params_file_path,
-            output_dir=self.output_dir)
+        # run_r_script_for_an_analyte(hashed_id='0c638508', csv_ds_root_path=self.csv_ds_root_path,
+        #                             params_file_path=params_file_path,
+        #                             output_dir=self.output_dir)
 
-        print('subcall:' + subprocess_call_for_r_script)
+        run_r_script_for_all_analytes(id_analyte_path=input_path,
+                                      csv_ds_root_path=self.csv_ds_root_path,
+                                      params_file_path=params_file_path,
+                                      output_dir=self.output_dir)
 
-        subprocess.call(subprocess_call_for_r_script, shell=True)
+        df = pd.read_csv(input_path)
+        df.to_csv(self.output().path)
 
-        print("Rtask end")
 
 
 # echo python$PATH
@@ -214,11 +215,28 @@ def generate_subprocess_call_for_a_analyte(hashed_id, csv_ds_root_path, params_f
 
     subprocess_call_for_r_script = str(
         R_SCRIPT_PATH +
-        ' "' + local_path + 'src/AvG_R_scripts/dummy_r_script.R' + '" ' +
+        ' "' + local_path + 'src/AvG_R_scripts/AvG_from_partitionedParquet.R' + '" ' +
         ' "' + local_path + csv_ds_root_path + 'ID_Analyte_' + str(hashed_id) + '.csv' + '" ' +
         ' "' + str(params_file_path) + '" ' +
         ' "' + str(hashed_id) + '" ' +
         ' "' + local_path + output_dir + '" ')
 
     return subprocess_call_for_r_script
+
+def run_r_script_for_an_analyte(hashed_id, csv_ds_root_path, params_file_path, output_dir):
+
+    subprocess_call_for_r_script = generate_subprocess_call_for_a_analyte(
+        hashed_id, csv_ds_root_path, params_file_path, output_dir)
+
+    # print('subcall:' + subprocess_call_for_r_script)
+
+    subprocess.call(subprocess_call_for_r_script, shell=True)
+
+def run_r_script_for_all_analytes(id_analyte_path,csv_ds_root_path, params_file_path, output_dir):
+    dd = pd.read_csv(id_analyte_path)
+    dd['ID_Analyte'].map(lambda x: run_r_script_for_an_analyte(
+        hashed_id=x,
+        csv_ds_root_path=csv_ds_root_path,
+        params_file_path=params_file_path,
+        output_dir=output_dir))
 
