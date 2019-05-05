6331124300f82a043b80f1a2c86a92c250c57f9b
Sebastian Vaca <svaca@broadinstitute.org>
2019-05-04 20:03:50 -0400

    Changed ReadHashIndexAndPartitionParquetFile

--- src/final_project/AvG_luigi_pipeline.py
+++ src/final_project/AvG_luigi_pipeline.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import luigi
 import hashlib
 import pprint
@@ -27,7 +28,7 @@ def hash_str(some_val, salt=''):
 def hash_value(some_val, salt=''):
     return hash_str(some_val, salt=salt).hex()[:8]
 
-def read_and_hash_index_parquetFile(input_path, path ):
+def read_hashindex_and_partition_parquetFile(input_path, rootpath ):
 
     df = pq.read_pandas(input_path).to_pandas()
 
@@ -45,8 +46,51 @@ def read_and_hash_index_parquetFile(input_path, path ):
 
     table = pa.Table.from_pandas(df)
     pq.write_to_dataset(table,
-                        root_path= path,
+                        root_path=rootpath,
                         partition_cols=['ID_Analyte'])
+
+    ## Create directory files to save the correspondance of the hash ids and the real values
+    df_ID_Analyte = df[['ID_Analyte',
+                        'Protein Name',
+                        'Peptide Modified Sequence',
+                        'Precursor Charge',
+                        'Is Decoy']].drop_duplicates()
+    df_ID_Analyte.to_csv(rootpath + "ID_Analyte.csv", index=False)
+
+    df_ID_FragmentIon_charge = df[['ID_FragmentIon_charge',
+                                   'Fragment Ion',
+                                   'Product Charge']].drop_duplicates()
+    df_ID_FragmentIon_charge.to_csv(rootpath + "ID_FragmentIon_charge.csv", index=False)
+
+    df_ID_Rep = df[['ID_Rep', 'File Name']].drop_duplicates()
+    df_ID_Rep.to_csv(rootpath + "ID_Rep.csv", index=False)
+
+
+def convert_csv_to_parquet_by_chunks(input_csv_path, parquet_file_path, chunksize):
+    csv_stream = pd.read_csv(input_csv_path,
+                             sep=',',
+                             chunksize=chunksize,
+                             low_memory=False)
+
+    for i, chunk in enumerate(csv_stream):
+        print("Chunk", i)
+        if i == 0:
+            # Guess the schema of the CSV file from the first chunk
+            parquet_schema = pa.Table.from_pandas(df=chunk).schema
+            # Open a Parquet file for writing
+            parquet_writer = pq.ParquetWriter(parquet_file_path, parquet_schema, compression='snappy')
+        # Write CSV chunk to the parquet file
+        table = pa.Table.from_pandas(chunk, schema=parquet_schema)
+        parquet_writer.write_table(table)
+        parquet_writer.close()
+
+def read_only_one_partition_and_write_csv(parquet_dataset_dirpath, output_dirpath, ID_analyte):
+    dataset = pq.ParquetDataset(parquet_dataset_dirpath,
+                                filters=[('ID_Analyte', '=', str(ID_analyte)), ])
+    df = dataset.read().to_pandas()
+    df.to_csv(output_dirpath+'ID_Analyte_'+ID_analyte+'.csv', index=False)
+
+
 
 class SaltString():
     @staticmethod
@@ -80,48 +124,45 @@ class ConvertCSVToParquet(luigi.Task):
         return luigi.LocalTarget("data/AvantGardeDIA_Export_%s.parquet" % hex_tag)
 
     def run(self):
-        chunksize = 20000
-        parquet_file = self.output().path
+        convert_csv_to_parquet_by_chunks(input_csv_path=self.input()['inputcsv'].path,
+                                         parquet_file_path=self.output().path,
+                                         chunksize=20000)
 
-        print(self.input()['inputcsv'].path)
-        print(parquet_file)
-
-        csv_stream = pd.read_csv(self.input()['inputcsv'].path,
-                                 sep=',',
-                                 chunksize=chunksize,
-                                 low_memory=False)
-
-        for i, chunk in enumerate(csv_stream):
-            print("Chunk", i)
-            if i == 0:
-                # Guess the schema of the CSV file from the first chunk
-                parquet_schema = pa.Table.from_pandas(df=chunk).schema
-                # Open a Parquet file for writing
-                parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
-            # Write CSV chunk to the parquet file
-            table = pa.Table.from_pandas(chunk, schema=parquet_schema)
-            parquet_writer.write_table(table)
-            parquet_writer.close()
-
-
-class ReadAndHashIndexParquetFile(luigi.Task):
-    file_stem_reduce = luigi.Parameter(default='reduced_data')
+class ReadHashIndexAndPartitionParquetFile(luigi.Task):
+    root_path = luigi.Parameter(default='data/pq_ds/', is_global=True)
 
     def requires(self):
         return ConvertCSVToParquet()
     def output(self):
         hex_tag = SaltString.get_hash_of_file(self.input().path)
-        return luigi.LocalTarget(self.file_stem_reduce+"_%s.txt" % hex_tag)
+        return luigi.LocalTarget(self.root_path+'_SUCCESS'+"_%s.txt" % hex_tag)
     def run(self):
-        read_and_hash_index_parquetFile(input_path=self.input().path,
-                                        path='data/pq_ds/')
+        read_hashindex_and_partition_parquetFile(input_path=self.input().path, rootpath=self.root_path)
+
+        with self.output().open('w') as out_file:
+            out_file.write("Done!" + "\n")
+
+class TransformParquetPartitionsToCSV(luigi.Task):
+    csv_ds_root_path = luigi.Parameter(default='data/csv_ds/', is_global=True)
+
+    def requires(self):
+        return ReadHashIndexAndPartitionParquetFile()
+    def output(self):
+        hex_tag = SaltString.get_hash_of_file(self.input().path)
+        return luigi.LocalTarget(self.root_path+'_SUCCESS'+"_%s.txt" % hex_tag)
+    def run(self):
+        read_hashindex_and_partition_parquetFile(input_path=self.input().path, rootpath=self.root_path)
+
+        with self.output().open('w') as out_file:
+            out_file.write("Done!" + "\n")
 
 
 class RTask(luigi.Task):
     file_stem_rtask=luigi.Parameter(default='rtask_data')
+    root_path = ReadHashIndexAndPartitionParquetFile.root_path
 
     def requires(self):
-        return MapStep()
+        return ReadHashIndexAndPartitionParquetFile()
 
     def output(self):
         hex_tag = SaltString.get_hash_of_file(self.input().path)
