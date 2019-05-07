4a7c61074843c4d2dde7e5ee2ac6d01ae8be8d51
Sebastian Vaca <svaca@broadinstitute.org>
2019-05-06 21:35:12 -0400

    Added documentation

--- src/final_project/luigi_avg_rtask_utils.py
+++ src/final_project/luigi_avg_rtask_utils.py
@@ -4,6 +4,18 @@ import os
 
 
 def generate_subprocess_call_for_a_analyte(hashed_id, csv_ds_root_path, params_file_path, output_dir):
+    """ Generates the command to be passed to the subprocess module.
+        The command is created for a given analyte.
+        :param str hashed_id: hashed id of the analyte
+        :param str csv_ds_root_path: path to the folder of temporary csv files
+        :param str params_file: path to the parameters file for the R script
+        :param str output_dir: output path of the 'ID_Analyte_glossary' file. This file contains the values of all
+        hashed ids and it is used as the _SUCCESS file
+
+        :returns: subprocess_call_for_r_script
+
+        """
+
     R_SCRIPT_PATH = os.getenv('R_SCRIPT_PATH')
     local_path = os.getenv('local_path')
 
@@ -18,6 +30,17 @@ def generate_subprocess_call_for_a_analyte(hashed_id, csv_ds_root_path, params_f
     return subprocess_call_for_r_script
 
 def run_r_script_for_an_analyte(hashed_id, csv_ds_root_path, params_file_path, output_dir):
+    """ Generates a command and passes it to the subprocess module.
+        The R script is run using the data for a analyte.
+
+        :param str hashed_id: hashed id of the analyte
+        :param str csv_ds_root_path: path to the folder of temporary csv files
+        :param str params_file: path to the parameters file for the R script
+        :param str output_dir: output path of the 'ID_Analyte_glossary' file. This file contains the values of all
+        hashed ids and it is used as the _SUCCESS file
+
+
+        """
 
     subprocess_call_for_r_script = generate_subprocess_call_for_a_analyte(
         hashed_id, csv_ds_root_path, params_file_path, output_dir)
@@ -27,6 +50,18 @@ def run_r_script_for_an_analyte(hashed_id, csv_ds_root_path, params_file_path, o
     subprocess.call(subprocess_call_for_r_script, shell=True)
 
 def run_r_script_for_all_analytes(id_analyte_path,csv_ds_root_path, params_file_path, output_dir):
+    """ Run the avant-garde R script for all analytes. Reads the 'ID_Analyte_glossary' file, that contains all
+    hashed id values for all the analytes and iterates through it.
+
+        :param str id_analyte_path: output path of the 'ID_Analyte_glossary' file. This file contains the values of all
+        hashed id values.
+        :param str hashed_id: hashed id of the analyte
+        :param str csv_ds_root_path: path to the folder of temporary csv files
+        :param str params_file_path: path to the parameters file for the R script
+        :param str output_dir: output path of the 'ID_Analyte_glossary' file that is used as a _SUCCESS file
+        to be written in R to make sure that the script ran without errors.
+
+        """
     dd = pd.read_csv(id_analyte_path)
     dd['ID_Analyte'].map(lambda x: run_r_script_for_an_analyte(
         hashed_id=x,
